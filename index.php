8f0d6fea6aec01d14a72fd94233a4f79a5f79892
cosimo <ballerini.co@gmail.com>
2024-09-24 09:35:54 +0200

    finito

---
+++ index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php
+        $ize = rand(16,60) . "px";
+
+        echo "<p style='font-size: $ize'>text</p>";
+
+        
+    ?>
+</body>
+</html>
